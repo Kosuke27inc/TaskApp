3c3ee9a9fa00b7f00d128497e905e92939829491
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-04-29 10:21:34 +0900

    さらに更新

--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -7,133 +7,58 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    
-    var tasks: Results<Task>? // Realmから取得したTaskオブジェクトの結果を保持
-    
-    //Realmインスタンスを取得する
-    let realm = try! Realm()
-    tasks = realm.objects(Task.self).sorted(byKeyPath: "date", ascending: true)
-}
 
-// テーブルビューの行数を返す
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return tasks?.count ?? 0
-}
+    // 15行目のエラー修正: taskArrayの宣言をクラスのプロパティに移動
+    var taskArray: Results<Task>? // DB内のタスクが格納されるリスト
 
-// 各行に表示するセルを返す
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    if let task = tasks?[indexPath.row] {
-        // セルにTaskの情報を表示
-        cell.textLabel?.text = task.title
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44.0 // 推定の行の高さ
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // 26行目のエラー修正: RealmのクエリをviewDidLoad内で実行
+        let realm = try! Realm()
+        taskArray = realm.objects(Task.self).sorted(byKeyPath: "date", ascending: true)
     }
-    return cell
-}
 
-// DB内のタスクが格納されるリスト。
-// 日付の近い順でソート：昇順
-// 以降内容をアップデートするとリスト内は自動的に更新される。
-var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)  // ←追加
-
-
-override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    tableView.rowHeight = UITableView.automaticDimension
-    tableView.delegate = self
-    tableView.dataSource = self
-}
-// segue で画面遷移する時に呼ばれる
-override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-    let inputViewController:InputViewController = segue.destination as! InputViewController
-    
-    if segue.identifier == "cellSegue" {
-        let indexPath = self.tableView.indexPathForSelectedRow
-        inputViewController.task = taskArray[indexPath!.row]
-    } else {
-        inputViewController.task = Task()
-    }
-}
-// 入力画面から戻ってきた時に TableView を更新させる
-override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    tableView.reloadData()
-}
-
-// データの数（＝セルの数）を返すメソッド
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return taskArray.count
-}
-
-// 各セルの内容を返すメソッド
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // 再利用可能な cell を得る
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    
-    // Cellに値を設定する  --- ここから ---
-    let task = taskArray[indexPath.row]
-    var content = cell.defaultContentConfiguration()
-    content.text = task.title
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd HH:mm"
-    let dateString:String = formatter.string(from: task.date)
-    content.secondaryText = dateString
-    cell.contentConfiguration = content
-    // --- ここまで追加 ---
-    
-    return cell
-}
-
-// 各セルを選択した時に実行されるメソッド
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    performSegue(withIdentifier: "cellSegue",sender: nil)
-}
-
-// セルが削除が可能なことを伝えるメソッド
-func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
-    return .delete
-}
-
-// Delete ボタンが押された時に呼ばれるメソッド
-func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    // --- ここから ---
-    if editingStyle == .delete {
-        // データベースから削除する
-        try! realm.write {
-            self.realm.delete(self.taskArray[indexPath.row])
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    } // --- ここまで追加 ---
-}
-import UserNotifications    // 追加
-// Delete ボタンが押された時に呼ばれるメソッド
-func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    // --- ここから ---
-    if editingStyle == .delete {
-        // 削除するタスクを取得する
-        let task = self.taskArray[indexPath.row]
-        
-        // ローカル通知をキャンセルする
-        let center = UNUserNotificationCenter.current()
-        center.removePendingNotificationRequests(withIdentifiers: [String(task.id.stringValue)])
-        
-        // データベースから削除する
-        try! realm.write {
-            self.realm.delete(task)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-        
-        // 未通知のローカル通知一覧をログ出力
-        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
-            for request in requests {
-                print("/---------------")
-                print(request)
-                print("---------------/")
+    // segueで画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 35行目のエラー修正: as! から安全なキャストに変更し、存在を確認
+        if let inputViewController = segue.destination as? InputViewController {
+            if segue.identifier == "cellSegue" {
+                if let indexPath = tableView.indexPathForSelectedRow {
+                    // ここでTaskオブジェクトを渡す
+                    inputViewController.task = taskArray?[indexPath.row]
+                }
+            } else {
+                // 38行目のエラー修正: 新しいTaskをInputViewControllerで処理するよう変更
+                inputViewController.task = Task()
             }
         }
-    } // --- ここまで変更 ---
-}
+    }
+
+    // 入力画面から戻ってきた時にTableViewを更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    // 以下、テーブルビューのデリゲートとデータソースメソッド
+    // ...
+
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    // Deleteボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // ...
+    }
 }
